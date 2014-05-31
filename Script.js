16497ce0212fea7f232ba14d6a85d9a0eea03355
JennaKrinsley <Jenna.Krinsley@gmail.com>
2014-05-31 15:47:40 -0400

    mom webpage

---
+++ Script.js
@@ -0,0 +1,5 @@
+
+if(!window.slider) {
+var slider={};
+slider.data=[{"id":"slide-img-1","client":"painting","desc":"ocean painting"},{"id":"slide-img-2","client":"nature beauty","desc":"add your description here"},{"id":"slide-img-3","client":"nature beauty","desc":"add your description here"},{"id":"slide-img-4","client":"nature beauty","desc":"add your description here"},{"id":"slide-img-5","client":"nature beauty","desc":"add your description here"},{"id":"slide-img-6","client":"nature beauty","desc":"add your description here"},{"id":"slide-img-7","client":"nature beauty","desc":"add your description here"}];
+}
